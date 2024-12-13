b44eeb6561b72d5e271801a2a30e541da31b22e5
Dustin Lacewell <dlacewell@gmail.com>
2024-12-13 15:09:15 -0600

    Add example module/widget

---
+++ Makefile
@@ -0,0 +1,27 @@
+# If RACK_DIR is not defined when calling the Makefile, default to two directories above
+RACK_DIR ?= ../..
+
+# print RACK_DIR
+$(info RACK_DIR is $(RACK_DIR))
+
+# FLAGS will be passed to both the C and C++ compiler
+FLAGS += -isystem $(RACK_DIR)/include
+CFLAGS +=
+CXXFLAGS += -Iinclude
+
+# Careful about linking to shared libraries, since you can't assume much about the user's environment and library search path.
+# Static libraries are fine, but they should be added to this plugin's build system.
+LDFLAGS +=
+
+# Add .cpp files to the build
+SOURCES += $(wildcard src/**/*.cpp)
+SOURCES += $(wildcard src/*.cpp)
+
+# Add files to the ZIP package when running `make dist`
+# The compiled plugin and "plugin.json" are automatically added.
+DISTRIBUTABLES += $(wildcard LICENSE*) res
+
+# Include the Rack plugin Makefile framework
+include $(RACK_DIR)/plugin.mk
+
+CXXFLAGS += -std=c++20
--- include/SvgHelper.hpp
+++ include/SvgHelper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <regex>
 
 #include "nanosvg.h"
@@ -7,16 +8,156 @@
 
 using namespace rack;
 
+template <typename T>
 struct SvgHelper {
 
-    std::string filename;
 
-    SvgHelper(std::string filename);
+    std::shared_ptr<Svg> svg;
 
-    Vec findNamed(std::string name);
-    std::vector<Vec> findPrefixed(std::string prefix);
-    std::vector<std::pair<std::vector<std::string>, Vec>> findMatched(std::string regex);
+    ModuleWidget* moduleWidget() {
+        auto t = static_cast<T*>(this);
+        return static_cast<ModuleWidget*>(t);
+    }
 
-    void forEachPrefixed(std::string prefix, std::function<void(int i, Vec)> callback);
-    void forEachMatched(std::string regex, std::function<void(std::vector<std::string> captures, Vec)> callback);
+    void loadPanel(const std::string& filename) {
+        if (svg == nullptr) {
+            DEBUG("Loading SVG file [%s]", filename.c_str());
+            // Load and parse the SVG file for the first time.
+            auto panel = createPanel(filename);
+            svg = panel->svg;
+            moduleWidget()->setPanel(panel);
+        } else if (svg != nullptr) {
+            DEBUG("Reloading SVG file [%s]", filename.c_str());
+            // Once loaded, VCV Rack caches the panel internally.
+            // We have to force it to reload and reparse the SVG file.
+            // Attempt to create a new SVG handle before replacing the one
+            // that exists. This way, in case the file is missing or corrupt,
+            // we don't lose the existing panel, nor do we risk crashing VCV Rack.
+            // This is quite likely during iterative development, which is why
+            // this code exists in the first place!
+            NSVGimage* replacement =
+                nsvgParseFromFile(filename.c_str(), "px", SVG_DPI);
+
+            if (replacement == nullptr) {
+                // Leave the existing panel in place, and log why it didn't change.
+                WARN("Cannot load/parse SVG file [%s]", filename.c_str());
+            } else {
+                // Successful reload. Destroy the old SVG and replace it with the new one.
+                if (svg->handle) {
+                    DEBUG("Deleting old SVG handle");
+                    nsvgDelete(svg->handle);
+                }
+
+                DEBUG("Replacing SVG handle");
+                svg->handle = replacement;
+                rack::ModuleWidget* widget = moduleWidget();
+                SvgPanel* panel = dynamic_cast<SvgPanel*>(widget->getPanel());
+                panel->fb->dirty = true;
+            }
+        } else {
+            // This should never happen. If it does, there is a bug I need to fix.
+            WARN("Weird! Somehow we lost our SVG panel.");
+        }
+    }
+
+    void forEachShape(const std::function<void(NSVGshape*)>& callback) {
+        auto shapes = svg->handle->shapes;
+        for (NSVGshape* shape = shapes; shape != nullptr; shape = shape->next) {
+            callback(shape);
+        }
+    }
+
+    // return the actual shape
+    std::optional<NSVGshape*> findShape(std::string name) {
+        NSVGshape* result = nullptr;
+
+        forEachShape([&](NSVGshape* shape) {
+            if (std::string(shape->id) == name) {
+                result = shape;
+                return;
+            }
+        });
+
+        return result;
+    }
+
+    std::optional<Vec> findNamed(std::string name) {
+        std::optional<Vec> result;
+
+        forEachShape([&](NSVGshape* shape) {
+            DEBUG("Shape id: %s", shape->id);
+            if (std::string(shape->id) == name) {
+                auto bounds = shape->bounds;
+                result = Vec();
+                result->x = (bounds[0] + bounds[2]) / 2;
+                result->y = (bounds[1] + bounds[3]) / 2;
+                return;
+            }
+        });
+
+        return result;
+    }
+
+    std::vector<Vec> findPrefixed(std::string prefix) {
+        std::vector<Vec> result;
+
+        forEachShape([&](NSVGshape* shape) {
+            if (std::string(shape->id).find(prefix) == 0) {
+                auto bounds = shape->bounds;
+                auto center =
+                    Vec((bounds[0] + bounds[2]) / 2,
+                        (bounds[1] + bounds[3]) / 2);
+                result.push_back(center);
+            }
+        });
+
+        return result;
+    }
+
+    std::vector<std::pair<std::vector<std::string>, Vec>> findMatched(
+        const std::string& pattern
+    ) {
+        std::regex regex(pattern);
+        std::vector<std::pair<std::vector<std::string>, Vec>> result;
+
+        forEachShape([&](NSVGshape* shape) {
+            auto id = std::string(shape->id);
+
+            std::vector<std::string> captures;
+            std::smatch match;
+
+            if (std::regex_search(id, match, regex)) {
+                for (unsigned int i = 1; i < match.size(); i++) {
+                    captures.push_back(match[i]);
+                }
+                auto bounds = shape->bounds;
+                auto center =
+                    Vec((bounds[0] + bounds[2]) / 2,
+                        (bounds[1] + bounds[3]) / 2);
+                result.emplace_back(captures, center);
+            }
+        });
+
+        return result;
+    }
+
+    void forEachPrefixed(
+        std::string prefix,
+        const std::function<void(unsigned int i, Vec)>& callback
+    ) {
+        auto positions = findPrefixed(prefix);
+        for (unsigned int i = 0; i < positions.size(); i++) {
+            callback(i, positions[i]);
+        }
+    }
+
+    void forEachMatched(
+        const std::string& regex,
+        const std::function<void(std::vector<std::string>, Vec)>& callback
+    ) {
+        auto matches = findMatched(regex);
+        for (const auto& match : matches) {
+            callback(match.first, match.second);
+        }
+    }
 };
